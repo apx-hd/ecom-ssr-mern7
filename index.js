86ec98ae4dbd8b0685b4e58a96f248e77f2a0931
Aabiskar Pandey <pandeyaabiskar@gmail.com>
2023-05-15 18:56:14 +0545

    Create REST API for ecommerce application

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const PORT = 4000;
+const homeRouter = require('./routes/home')
+const productRouter = require('./routes/product')
+const errorRouter = require('./routes/error')
+
+const app = express();
+
+// Link the routes file
+app.use(homeRouter)
+app.use("/api/products", productRouter)
+app.use(errorRouter)
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`)
+})
+
+console.log("Test")
---
+++ routes/product.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const productData = require("../data/products.json");
+
+//Initialize router object
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  const { category, minprice } = req.query;
+  //Apply filter here
+  if (category && minprice) {
+    const filteredData = productData.filter((element) => {
+      return element.category === category && element.price >= minprice;
+    });
+    res.json(filteredData);
+  } else if (category) {
+    const filteredData = productData.filter((element) => {
+      return element.category === category;
+    });
+    res.json(filteredData);
+  } else if (minprice) {
+    const filteredData = productData.filter((element) => {
+      return element.price >= minprice;
+    });
+    res.json(filteredData);
+  } else res.json(productData);
+
+});
+
+router.get("/:productID", (req, res) => {
+  console.log(req.params);
+  const { productID } = req.params;
+  const product = productData.find(
+    (product) => product.id === Number(productID)
+  );
+  res.json(product ? product : "Index Not Found");
+});
+
+router.post("/", (req, res) => {
+  res.send("This api will create product in database");
+});
+
+router.put("/:productID", (req, res) => {
+  res.send("This api will replace product in database");
+});
+
+router.patch("/:productID", (req, res) => {
+  res.send("This api will update product in database");
+});
+
+router.delete("/:productID", (req, res) => {
+  res.send("This api will delete product in database");
+});
+
+module.exports = router;
